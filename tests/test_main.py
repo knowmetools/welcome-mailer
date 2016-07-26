f8a8e0c9616decd9c6cfd75a14d9f9abab4d8765
Chathan Driehuys <cdriehuys@gmail.com>
2016-07-25 22:31:16 -0400

    Adding tests for 'lambda_handler' function.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from mock import patch
+
 import local_settings as settings
 import main
 from testing_utils import create_user
@@ -17,6 +19,38 @@ class TestGetClient(TestCase):
         client = main.get_client()
 
         self.assertEqual(settings.API_KEY, client.apikey)
+
+
+@patch('main.send_email', autospec=True, return_value={})
+class TestLambdaHandler(TestCase):
+    """ Test cases for the lambda_handler function """
+
+    def test_new_user(self, mock_send_email):
+        """ Test sending a new user event to the handler.
+
+        If a new user event is sent to the handler, an email should be
+        sent to the new user.
+        """
+        event = fixtures.new_user_event
+        user = main.parse_user(event)
+
+        result = main.lambda_handler(event, None)
+
+        self.assertEqual({}, result)
+        mock_send_email.assert_called_with(user)
+
+    def test_update(self, mock_send_email):
+        """ Test sending an update event to the handler.
+
+        If the event is a user being updated, no mail should be sent,
+        and a blank dictionary should be returned.
+        """
+        event = fixtures.update_user_event
+
+        result = main.lambda_handler(event, None)
+
+        self.assertEqual({}, result)
+        self.assertEqual(0, mock_send_email.call_count)
 
 
 class TestParseUser(TestCase):
